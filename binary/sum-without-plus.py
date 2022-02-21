3f19614c8a42c3daba2e6a891b3daafbbf0d45da
David Naughton <57878980+naughtod@users.noreply.github.com>
2022-02-21 20:49:05 +1100

    Update sum-without-plus.py

--- binary/sum-without-plus.py
+++ binary/sum-without-plus.py
@@ -7,11 +7,11 @@ class Solution(object):
         """
         mask = 0xffffffff
         while b:
-			sum = (a^b) & mask
-			carry = ((a&b)<<1) & mask
-            a = sum
-			b = carry
+		sum = (a^b) & mask
+		carry = ((a&b)<<1) & mask
+            	a = sum
+		b = carry
 
-		if (a>>31) & 1: 
-			return ~(a^mask)
-		return a
+	if (a>>31) & 1: 
+		return ~(a^mask)
+	return a
