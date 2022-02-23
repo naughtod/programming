734d3c862a7928b34ab0cb3b16d0bb37c0fa1e6e
David Naughton <57878980+naughtod@users.noreply.github.com>
2022-02-23 13:29:52 +1100

    Update valid-palindrome.java

--- string/valid-palindrome.java
+++ string/valid-palindrome.java
@@ -3,9 +3,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i=0;i<s.length();i++) {
-            if (Character.isLetterOrDigit(s.charAt(i))) {
+            if (Character.isLetterOrDigit(s.charAt(i))) 
                 sb.append(Character.toLowerCase(s.charAt(i)));    
-            }
         }
         
         String original = sb.toString(); 
