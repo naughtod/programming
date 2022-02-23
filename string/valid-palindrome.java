c3cc74ac8e739b9831719ea335debc98a1bed952
David Naughton <57878980+naughtod@users.noreply.github.com>
2022-02-23 13:29:24 +1100

    Create valid-palindrome.java

---
+++ string/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=0;i<s.length();i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                sb.append(Character.toLowerCase(s.charAt(i)));    
+            }
+        }
+        
+        String original = sb.toString(); 
+        String reversed = sb.reverse().toString();
+        
+        return original.equals(reversed);
+    }
+}
